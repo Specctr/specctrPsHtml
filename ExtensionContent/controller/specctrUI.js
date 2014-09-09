45ea56a3aba760a394c64567a337461059f9e457
Vaibhav Goel <vaibhavg@mindfiresolutions.com>
2014-09-09 17:34:43 +0530

    Completed the work of config, license and log file.

--- ExtensionContent/controller/specctrUI.js
+++ ExtensionContent/controller/specctrUI.js
@@ -4,43 +4,43 @@ Description: Includes all the methods related to UI component like change event 
  */
 
 /**
- * FunctionName	: activateButton_clickHandler()
- * Description	: Validate the license of the user and move to the tab container if user's credentials valid.
+ * Validate the license of the user and move to the tab container
+ * if user's credentials valid.
  * */
-function activateButton_clickHandler()
-{
-	var urlRequest;
-
+function activateButton_clickHandler() {
 	// Get Extension Id and matching productCode.
 	var productCodes = {
-			"SpecctrPs-Pro":"31265"};
+		"SpecctrPs-Pro" : "31265"
+	};
 
 	var csInterface = new CSInterface();
 	var extensionId = csInterface.getExtensionID();
+	var logData = "";
 
-	//If no installed extension is matched with productCodes values.
-	if(!extensionId)
-	{
-		alert("Incorrect product code!");
+	// If no installed extension is matched with productCodes values.
+	if (!productCodes[extensionId]) {
+		logData = createLogData("Incorrect product code");
+		addFileToPreferenceFolder('.log', logData);	//Create log file.
 		return;
 	}
 
-	urlRequest = "http://specctr-license.herokuapp.com";
+	var urlRequest = "http://specctr-license.herokuapp.com";
 	urlRequest += "?product=" + productCodes[extensionId];
 	urlRequest += "&license=" + document.getElementById("license").value;
 	urlRequest += "&email=" + document.getElementById("emailInput").value;
 
-	$.get(urlRequest, completeHandler);		
+	$.get(urlRequest, completeHandler).error(function() {
+		logData = createLogData("No connection available");
+		addFileToPreferenceFolder('.log', logData);	//Create log file.
+	});
 }
 
 /**
- * FunctionName	: tab_clickHandler()
- * Description	: Click event handler of tabs. Modify styles to tabs and call the function to change images on tab.
+ * Click event handler of tabs. Modify styles to tabs and 
+ * call the function to change images on tab.
  * */
-function tab_clickHandler() 
-{
-	try
-	{
+function tab_clickHandler() {
+	try {
 		var ident = this.id.split("_")[1];
 		var current = this.parentNode.getAttribute("data-current");
 
@@ -48,13 +48,11 @@ function tab_clickHandler()
 		var currentTabHeader = document.getElementById("tabHeader_" + current);
 		var currentTabPage = document.getElementById("tabpage_" + current);
 
-		if(currentTabHeader && currentTabPage && (ident >= "1" && ident <= "3"))
-		{
+		if (currentTabHeader && currentTabPage
+				&& (ident >= "1" && ident <= "3")) {
 			currentTabHeader.removeAttribute("class");
 			currentTabPage.style.display = "none";
-		}
-		else
-		{
+		} else {
 			return false;
 		}
 
@@ -66,9 +64,7 @@ function tab_clickHandler()
 
 		document.getElementById("tabpage_" + ident).style.display = "block";
 		this.parentNode.setAttribute("data-current", ident);
-	}
-	catch(e)
-	{
+	} catch (e) {
 		console.log(e);
 		return false;
 	}
@@ -77,13 +73,11 @@ function tab_clickHandler()
 }
 
 /**
- * FunctionName	: changeImagesOfTabs()
- * Description	: Change the images of tabs on their selection.
+ * Change the images of tabs on their selection.
+ * @param selectedTab {number} The position of selected tab.
  * */
-function changeImagesOfTabs(selectedTab)
-{
-	try
-	{
+function changeImagesOfTabs(selectedTab) {
+	try {
 		var tab1ImagePath = "../Images/Icon_main";
 		var tab2ImagePath = "../Images/Icon_settings";
 		var tab3ImagePath = "../Images/Icon_prefs";
@@ -95,15 +89,14 @@ function changeImagesOfTabs(selectedTab)
 		var tab2Image = document.getElementById("tabImage_2");
 		var tab3Image = document.getElementById("tabImage_3");
 
-		switch(selectedTab) 
-		{
-		case 1: 
+		switch (selectedTab) {
+		case 1:
 			tab1Image.src = tab1ImagePath + activeImageEndString;
 			tab2Image.src = tab2ImagePath + imageExtension;
 			tab3Image.src = tab3ImagePath + imageExtension;
 			break;
 
-		case 2: 
+		case 2:
 			tab1Image.src = tab1ImagePath + imageExtension;
 			tab2Image.src = tab2ImagePath + activeImageEndString;
 			tab3Image.src = tab3ImagePath + imageExtension;
@@ -117,11 +110,9 @@ function changeImagesOfTabs(selectedTab)
 
 		default:
 			isImageChanged = false;
-		} 
+		}
 
-	}
-	catch(e)
-	{
+	} catch (e) {
 		isImageChanged = false;
 	}
 
@@ -129,150 +120,101 @@ function changeImagesOfTabs(selectedTab)
 }
 
 /**
- * FunctionName	: checkBox_changeHandler()
- * Description	: Set the value of checkBox model value when changed.
- * */
-function checkBox_changeHandler(checkBoxId)
-{
-	try
-	{
-		model[checkBoxId] = document.getElementById(checkBoxId).checked;
-	}
-	catch(e)
-	{
-		console.log(e);	//For debugging.
-	}
+ * Set the value of checkBox model value when changed.
+ * @param checkBoxId {string} The id of selected checkbox.
+ */
+function checkBox_changeHandler(checkBoxId) {
+	model[checkBoxId] = document.getElementById(checkBoxId).checked;
+	writeAppPrefs();
 }
 
 /**
- * FunctionName	: textBox_changeHandler()
- * Description	: Set the model value of text box when changed.
- * */
-function textBox_changeHandler(textBoxId)
-{
-	try
-	{
-		model[textBoxId] = Number(document.getElementById(textBoxId).value);
-	}
-	catch(e)
-	{
-		console.log(e);	//For debugging.
-	}
+ * Set the model value of text box when changed.
+ * @param textBoxId {string} The id of selected text box.
+ */
+function textBox_changeHandler(textBoxId) {
+	model[textBoxId] = Number(document.getElementById(textBoxId).value);
 }
 
 /**
- * FunctionName	: radioButton_clickHandler()
- * Description	: Set the value of legendColorMode when selection of radio button changed.
- * */
-function radioButton_clickHandler(event)
-{
-	try
-	{
-		var colorMode = event.target.value;
-		if(colorMode != undefined)
-			model.legendColorMode	= colorMode;
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+ * Set the value of legendColorMode when selection of radio button changed.
+ * @param event {event type} The event comes after clicking radio button.
+ */
+function radioButton_clickHandler(event) {
+	var colorMode = event.target.value;
+	if (colorMode === undefined)
+		return;
+
+	model.legendColorMode = colorMode;
+	writeAppPrefs();
 }
 
 /**
- * FunctionName	: comboBox_changeHandler()
- * Description	: Set the value of font size when changed.
- * */
-function comboBox_changeHandler(elementId, modelValue)
-{
-	try
-	{
-		var comboHandler = document.getElementById(elementId);
-		model[modelValue] = Number(comboHandler.options[comboHandler.selectedIndex].value);
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+ * Set the value of font size when changed.
+ * @param elementId {string} The id of selected combobox.
+ * @param modelValue {string} The combobox name.
+ */
+function comboBox_changeHandler(elementId, modelValue) {
+	var comboHandler = document.getElementById(elementId);
+	model[modelValue] = Number(comboHandler.options[comboHandler.selectedIndex].value);
+	writeAppPrefs();
 }
 
 /**
- * FunctionName	: lstFont_changeHandler()
- * Description	: Set the value of font when changed.
- * */
-function lstFont_changeHandler()
-{
-	try
-	{
-		var fontListHandler = document.getElementById("lstFont");
-		model.legenFontIndex = Number(fontListHandler.options[fontListHandler.selectedIndex].value);
-		model.legendFont = fontListHandler.options[fontListHandler.selectedIndex].text;
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+ * Set the value of font when changed.
+ */
+function lstFont_changeHandler() {
+	var fontListHandler = document.getElementById("lstFont");
+	model.legenFontIndex = Number(fontListHandler.options[fontListHandler.selectedIndex].value);
+	model.legendFont = fontListHandler.options[fontListHandler.selectedIndex].text;
+	writeAppPrefs();
 }
 
 /**
- * FunctionName	: setColorValueToTextBox()
- * Description	: Set color value to the color-picker dropdown's input text on hovering the color blocks.
- * */
-function setColorValueToTextBox(element, colorPickerBlock)
-{
-	try
-	{
-		var inputTextName = "txt" + colorPickerBlock + "Color";
-		var blockName = colorPickerBlock.toLowerCase() + "ColorBlock";
+ * Set color value to the color-picker dropdown's input text on hovering the color blocks.
+ * @param element {object} The reference of color picker.
+ * @param colorPickerBlock {string} The name of color picker block.
+ */
+function setColorValueToTextBox(element, colorPickerBlock) {
+	var inputTextName = "txt" + colorPickerBlock + "Color";
+	var blockName = colorPickerBlock.toLowerCase() + "ColorBlock";
 
-		document.getElementById(inputTextName).value = element.title;
-		document.getElementById(blockName).style.backgroundColor = "#" + element.title; 
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+	document.getElementById(inputTextName).value = element.title;
+	document.getElementById(blockName).style.backgroundColor = "#"
+			+ element.title;
 }
 
 /**
- * FunctionName	: setColorToLabel()
- * Description	: Set color to the color picker's label on clicking any color block and close the dropdown.
- * */
-function setColorToLabel(element, colorPickerBlock)
-{
-	try
-	{
-		var labelName = "col" + colorPickerBlock;
-		var value = "legendColor" + colorPickerBlock;
-		
-		if(element.title)
-		{
-			var color = "#" + element.title;
-		}
-		else
-		{
-			color = document.getElementById(element.id).style.backgroundColor;
-			color = rgbToHex(color);
-		}
-			
-		document.getElementById(labelName).style.backgroundColor = color;
-		$("#" + element.parentNode.id).slideUp(100);
-		model[value] = color;
+ * Set color to the color picker's label on clicking any color block and 
+ * close the dropdown.
+ * @param element {object} The reference of color picker.
+ * @param colorPickerBlock {string} The name of color picker block.
+ */
+function setColorToLabel(element, colorPickerBlock) {
+	var labelName = "col" + colorPickerBlock;
+	var value = "legendColor" + colorPickerBlock;
+
+	if (element.title) {
+		var color = "#" + element.title;
+	} else {
+		color = document.getElementById(element.id).style.backgroundColor;
+		color = rgbToHex(color);
 	}
-	catch(e)
-	{
-		console.log(e);
-	}
+
+	document.getElementById(labelName).style.backgroundColor = color;
+	$("#" + element.parentNode.id).slideUp(100);
+	model[value] = color;
+	writeAppPrefs();
 }
 
 /**
- * FunctionName	: colObject_clickHandler()
- * Description	: Click handler of shape color-picker. Toggle the dropdown and set the color to 
- * the main color block in color picker.
- * */
-function colorPicker_clickHandler(elementId, colorPickerBlock)
-{
-	try
-	{
+ * Click handler of shape color-picker. Toggle the dropdown and 
+ * set the color to the main color block in color picker.
+ * @param elementId {string} The id of color picker.
+ * @param colorPickerBlock {string} The name of color picker block.
+ */
+function colorPicker_clickHandler(elementId, colorPickerBlock) {
+	try {
 		var colorBlockTextbox = "#txt" + colorPickerBlock + "Color";
 		var dropDownBlock = "#color" + colorPickerBlock + "DropDown";
 		var colorBlock = colorPickerBlock.toLowerCase() + "ColorBlock";
@@ -282,42 +224,34 @@ function colorPicker_clickHandler(elementId, colorPickerBlock)
 		color = rgbToHex(color);
 		$(dropDownBlock).slideToggle(100);
 		document.getElementById(colorBlock).style.backgroundColor = color;
-		
-		if(color[0] == "#")
+
+		if (color[0] == "#")
 			valueForTextBox = color.substring(1);
 		else
 			valueForTextBox = color;
-		
+
 		$(colorBlockTextbox).val(valueForTextBox.toUpperCase());
 
-		if(colorPickerBlock == "Shape")
-		{
+		if (colorPickerBlock == "Shape") {
 			model.legendColorObject = color;
 			$("#colorSpaceDropDown").slideUp(100);
 			$("#colorTypeDropDown").slideUp(100);
-		}
-		else if(colorPickerBlock == "Type")
-		{
+		} else if (colorPickerBlock == "Type") {
 			model.legendColorType = color;
 			$("#colorSpaceDropDown").slideUp(100);
-		}
-		else
-		{
+		} else {
 			model.legendColorSpacing = color;
 		}
-	}
-	catch(e)
-	{
+	} catch (e) {
 		console.log(e);
 	}
 }
 
 /**
- * FunctionName	: colorPickerTextInput_validation()
- * Description	: Validate the input value in text input of color picker block.
- * */
-function colorPickerTextInput_validation(event)
-{
+ * Validate the input value in text input of color picker block.
+ * @param event {event type} The event dispatched after clicking text input.
+ */
+function colorPickerTextInput_validation(event) {
 	var charCode = (event.which) ? event.which : event.keyCode;
 
 	//Allows only hex value like eeffee or #eeffee.
@@ -326,90 +260,73 @@ function colorPickerTextInput_validation(event)
 	 *			= 39 : Right arrow key
 	 *			= 46 : Delete key 
 	 **/
-	if((charCode < 48 && charCode != 8 && charCode != 37 && charCode != 39 && charCode != 46) 
-			|| (charCode > 57 && charCode < 65)	|| (charCode > 70 && charCode < 97) 
-				|| (charCode > 102) || (event.shiftKey && charCode != 51))
-	{
+	if ((charCode < 48 && charCode != 8 && charCode != 37 && charCode != 39 && charCode != 46)
+			|| (charCode > 57 && charCode < 65)
+			|| (charCode > 70 && charCode < 97)
+			|| (charCode > 102)
+			|| (event.shiftKey && charCode != 51)) {
 		return false;
-	}
-	else
-	{
+	} else {
 		return true;
 	}
 }
 
 /**
- * FunctionName	: colorPickerTextInput_clickHandler()
- * Description	: Change the color on text input in color picker block .
- * */
-function colorPickerTextInput_clickHandler(event, colorPickerBlock)
-{
+ * Change the color on text input in color picker block.
+ * @param event {event type} The keyDown event dispatched after pressing key
+ * in textbox.
+ * @param colorPickerBlock {string} The name of color picker block. 
+ */
+function colorPickerTextInput_clickHandler(event, colorPickerBlock) {
 	var elementId = "txt" + colorPickerBlock + "Color";
 	var textBox = document.getElementById(elementId);
 	var color = textBox.value;
-	var inputSize = 7;			//Length of the input hex value.
+	var inputSize = 7; //Length of the input hex value.
 
-	if(color.charAt(0) == "#")
-	{
+	if (color.charAt(0) == "#") {
 		textBox.maxLength = inputSize;
-	}
-	else
-	{
+	} else {
 		textBox.maxLength = inputSize - 1;
 		color = "#" + color;
 	}
 
-	for(var i = color.length; i < inputSize; i++)
+	for (var i = color.length; i < inputSize; i++)
 		color += "0";
 
 	var colorBlock = colorPickerBlock.toLowerCase() + "ColorBlock";
 	var mainColorBlock = document.getElementById(colorBlock);
 	mainColorBlock.style.backgroundColor = color;
 
-	if(event.keyCode == 13)		//Dispatch the click event if pressed key is 'Enter'.
+	if (event.keyCode == 13) //Dispatch the click event if pressed key is 'Enter'.
 		mainColorBlock.click();
 }
 
 /**
- * FunctionName	: text_KeyDownHandler()
- * Description	: Dispatch the click event of activeButton.
- * */
-function text_KeyDownHandler(event)
-{
-	try
-	{
-		if(event.keyCode == 13) 
-			document.getElementById("activateButton").click();
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+ * Dispatch the click event of activeButton.
+ * @param event {event type} The keyDown event dispatched after pressing key
+ * in textbox. 
+ */
+function text_KeyDownHandler(event) {
+	if (event.keyCode == 13)
+		document.getElementById("activateButton").click();
 }
 
 /**
- * FunctionName	: restrictInputToDecimal()
- * Description	: Restrict the text input to decimal value.
- * */
-function restrictInputToDecimal(event)
-{
-	try
-	{
-		var charCode = (event.which) ? event.which : event.keyCode;
+ * Restrict the text input to decimal value.
+ * @param event {event type} The keyDown event dispatched after pressing key
+ * in textbox. 
+ */
+function restrictInputToDecimal(event) {
+	var charCode = (event.which) ? event.which : event.keyCode;
 
-		if(charCode == 48 && !event.srcElement.value.length)
-			return false;
+	if (charCode == 48 && !event.srcElement.value.length)
+		return false;
 
-		if(charCode == 46 && event.srcElement.value.split(".").length>1)
-			return false;
+	if (charCode == 46 && event.srcElement.value.split(".").length > 1)
+		return false;
 
-		if(charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
-			return false;
+	if (charCode != 46 && charCode > 31 && (charCode < 48 || charCode > 57))
+		return false;
 
-		return true;
-	}
-	catch(e)
-	{
-		console.log(e);
-	}
+	return true;
 }
