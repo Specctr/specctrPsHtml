da65efe1d99aba950a2c1cd66adef46dfd6e3cb7
Vaibhav Goel <vaibhavg@mindfiresolutions.com>
2014-10-16 18:46:03 +0530

    Added swatch color name feature to property specs.

--- ExtensionContent/jsx/specctrPhotoshop.jsx
+++ ExtensionContent/jsx/specctrPhotoshop.jsx
@@ -5,6 +5,7 @@
 //////////////////////////////////////////////////////////////////////////////*/
 
 #include "json2.js"
+#include "stream.js"
 
 var model;
 var heightChoice = { "Left" : 1 , "Right" : 2, "Center" : 3 };
@@ -2546,7 +2547,11 @@ function getSpecsInfoForPathItem(pageItem)
             if(pathItem.kind == LayerKind.SOLIDFILL)
             {
                 var colorDescriptor = desc.getObjectValue(stringIDToTypeID('color'));
-                var color = colorAsString(getColor(colorDescriptor));
+                var solidColor = getColor(colorDescriptor);
+                var color = colorAsString(solidColor);
+                var swatchName = readFromRuntime(solidColor);
+                if(swatchName !== undefined)
+                    infoText +="\r" + swatchName;
                 var cssColor = "";
                 
                 infoText += "\rBackground: ";
@@ -3304,6 +3309,181 @@ function getRoundCornerValue()
     return infoText;
 }
 
+function readFromRuntime(color) 
+{
+    var file = new File("G:" + '\\' + "temp.aco");
+    if (file.exists)
+        file.remove();
+    saveSwatches(file);
+    var swatchName = readFromACOFile(file, color);
+    file.remove();
+    return swatchName;
+}
+
+function saveSwatches(fptr) {
+  var desc = new ActionDescriptor();
+  desc.putPath( charIDToTypeID('null'), fptr);
+  var ref = new ActionReference();
+  ref.putProperty(charIDToTypeID('Prpr'), charIDToTypeID('Clrs'));
+  ref.putEnumerated(charIDToTypeID('capp'), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+  desc.putReference(charIDToTypeID('T   '), ref);
+  executeAction(charIDToTypeID('setd'), desc, DialogModes.NO);
+};
+
+function readFromACOFile(fptr, color) {
+        var str = Stream.readStream(fptr);
+        var swatchName = read(str, color);
+        return swatchName;
+};
+
+function read(str, color) {
+  var version = str.readInt16();
+  var swatchName = '';
+  if (version == 1) {
+    var version1 = 1;
+    var numberOfColors1 = str.readInt16();
+    var count1 = 0;
+    swatchName = readColors(str, numberOfColors1, false, color);
+    if (str.eof())
+      return;
+    version = str.readInt16();
+  }
+
+  var version2 = 2;
+  var numberOfColors2 = str.readInt16();
+  var count2 = 0;
+  swatchName = readColors(str, numberOfColors2, true, color);
+  
+  return swatchName;
+}
+
+function readColors(str, count, names, color) {
+    var model = color.model;
+    for (var i = 0; i < count; i++) {
+    var cname;
+    var cbytes = [];
+    var ctype = str.readInt16();
+    cbytes.push(str.readInt16());
+    cbytes.push(str.readInt16());
+    cbytes.push(str.readInt16());
+    cbytes.push(str.readInt16());
+
+    if (names) {
+        try
+        {
+            cname = str.readUnicode();
+            var swatchColor = setColor(cbytes, ctype);
+
+            switch(model)
+             {
+                 case ColorModel.GRAYSCALE:
+                    var scGray = Math.round(swatchColor.gray.gray);     //swatch color Gray
+                    var ocGray = Math.round(color.gray.gray);               //object color Gray
+                     if( Math.abs(ocGray - scGray) <= 1)
+                        return cname;
+                     break;
+                 case ColorModel.RGB:
+//~                  alert(cname + ": "+ swatchColor.rgb.red + "_" +  swatchColor.rgb.green + "_"+  swatchColor.rgb.blue +"\r"
+//~                             + color.rgb.red + "_" + color.rgb.green +"_"+ color.rgb.blue);
+                     var scRed = Math.round(swatchColor.rgb.red), ocRed = Math.round(color.rgb.red);
+                     var scGreen = Math.round(swatchColor.rgb.green),  ocGreen = Math.round(color.rgb.green);
+                     var scBlue = Math.round(swatchColor.rgb.blue), ocBlue = Math.round(color.rgb.blue);
+                     if((Math.abs(ocRed - scRed) <= 1) && 
+                        (Math.abs(ocGreen - scGreen) <= 1) && 
+                        (Math.abs(ocBlue - scBlue) <= 1))
+                            return cname;
+                     break;
+                 case ColorModel.CMYK:
+//~                   alert(cname + ": "+ swatchColor.cmyk.cyan + "_" +  swatchColor.cmyk.magenta + "_"+  swatchColor.cmyk.yellow +"\r"
+//~                             + color.cmyk.cyan + "_" + color.cmyk.magenta +"_"+ color.cmyk.yellow);
+                     var scCyan = Math.round(swatchColor.cmyk.cyan), ocCyan = Math.round(color.cmyk.cyan);
+                     var scMagenta = Math.round(swatchColor.cmyk.magenta), ocMagenta = Math.round(color.cmyk.magenta);
+                     var scYellow = Math.round(swatchColor.cmyk.yellow), ocYellow = Math.round(color.cmyk.yellow);
+                     var scBlack = Math.round(swatchColor.cmyk.black), ocBlack = Math.round(color.cmyk.black);
+                     
+                      if((Math.abs(ocCyan - scCyan) <= 1) && 
+                      (Math.abs(ocMagenta - scMagenta) <= 1) && 
+                      (Math.abs(ocYellow - scYellow) <= 1) && 
+                      (Math.abs(ocBlack - scBlack) <= 1))
+                        return cname;
+                      break;
+                 case ColorModel.LAB:
+//~                      alert(cname + ": "+ swatchColor.lab.l + "_" +  swatchColor.lab.a + "_"+  swatchColor.lab.b +"\r"
+//~                             + color.lab.l+ "_" + color.lab.a +"_"+ color.lab.b);
+                        var scL = Math.round(swatchColor.lab.l), ocL = Math.round(color.lab.l);
+                        var scA = Math.round(swatchColor.lab.a), ocA = Math.round(color.lab.a);
+                        var scB = Math.round(swatchColor.lab.b), ocB = Math.round(color.lab.b);
+                        if((Math.abs(ocL - scL) <= 1) && 
+                        (Math.abs(ocA - scA) <= 1) && 
+                        (Math.abs(ocB - scB) <= 1))
+                            return cname;
+               }
+        }
+        catch(e)
+        {}
+    }
+  }
+}
+
+function setColor(cwords, ctype) {
+  var color = new SolidColor();
+
+  function cnvt256(v) {
+    var rc = v/256.0;
+    return (rc > 255.0) ? 255 : rc; // XXX this seems a bit odd (bug)
+  }
+  function cnvt100(v) {
+    return 100.0 * v / 0xFFFF;
+  }
+
+  switch (ctype) {
+    case 0:
+      color.rgb.red   = cnvt256(cwords[0]);
+      color.rgb.green = cnvt256(cwords[1]);
+      color.rgb.blue  = cnvt256(cwords[2]);
+      break;
+
+    case 1:
+      color.hsb.hue = 360 * cwords[0]/0xFFFF;
+      color.hsb.saturation = cnvt100(cwords[1]);
+      color.hsb.brightness  = cnvt100(cwords[2]);
+      break;
+
+    case 2:
+      color.cmyk.cyan    = 100 - cnvt100(cwords[0]);
+      color.cmyk.magenta = 100 - cnvt100(cwords[1]);
+      color.cmyk.yellow  = 100 - cnvt100(cwords[2]);
+      color.cmyk.black   = 100 - cnvt100(cwords[3]);
+      break;
+
+    case 7:
+      cwords[0]  = cwords[0] / 100;
+
+      if (cwords[1] & 0x8000) {
+        cwords[1] = (cwords[1]-0xFFFF) / 100;
+      } else {
+        cwords[1] = cwords[1] / 100;
+      }
+
+      if (cwords[2] & 0x8000) {
+        cwords[2] = (cwords[2]-0xFFFF) / 100;
+      } else {
+        cwords[2] = cwords[2] / 100;
+      }
+
+      color.lab.l = cwords[0];
+      color.lab.a = cwords[1];
+      color.lab.b = cwords[2];
+      break;
+
+    case 8:
+      color.gray.gray = cwords[0] / 100;
+      break;
+  }
+
+  return color;
+}
+
 //To get the type units of the application preferences.
 function getTypeUnits()
 {
@@ -3350,6 +3530,7 @@ function colorAsString(c)
 {
 	var result = "";
 	var color = c;
+  // alert( color.rgb.red + "_" +  color.rgb.green + "_"+  color.rgb.blue );
 	var newColor;
 	var colorType;
 	var foreGroundColor = app.foregroundColor;
