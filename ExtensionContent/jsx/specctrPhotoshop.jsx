cac227d59e9d61f2b596401b6df98004f3128a1e
Vaibhav Goel <vaibhavg@mindfiresolutions.com>
2015-02-24 19:11:34 +0530

    Added specctrPhotoshop.jsx.

---
+++ ExtensionContent/jsx/specctrPhotoshop.jsx
@@ -0,0 +1,69 @@
+﻿/*//////////////////////////////////////////////////////////////////////////////
+ * File Name: specctrPhotoshop.jsx
+ * Description: This file calls the spec methods from their respective files i.e. property spec,
+    width/height spec, spacing spec, coordinate spec and expand canvas feature.
+//////////////////////////////////////////////////////////////////////////////*/
+
+#target photoshop
+#include "specctrPsCommon.jsx"
+#include "specctrPsCoordinates.jsx"
+#include "specctrPsDimension.jsx"
+#include "specctrPsExpandCanvas.jsx"
+#include "specctrPsExportCss.jsx"
+#include "specctrPsProperties.jsx"
+#include "specctrPsSpacing.jsx"
+
+$.specctrPs = {
+    //Get the application font's name and font's family.
+    getFontList : function() {
+        var font = app.fonts;
+        var appFontLength = font.length;
+        var result = [];
+        //Set the spec text properties.
+        for(var i = 0; i < appFontLength; i++) {
+            var currFont = font[i];
+            if(currFont.style == "Regular") {
+                var object = {};
+                object.label = currFont.family;
+                object.font = currFont.postScriptName;
+                result.push(object);
+            }
+        }
+        return JSON.stringify(result);
+    },
+
+    //Call the setModel from specctrPsCommon..
+    setModel : function(currModel) {
+        $.specctrPsCommon.setModel(currModel);
+    },
+
+    //Call create canvas border method from specctrPsExpandCanvas jsx file.
+    createCanvasBorder : function() {
+        $.specctrPsExpandCanvas.createCanvasBorder();
+    },
+
+    //Call create dimension spec method from specctrPsDimension jsx file.
+    createDimensionSpecs : function() {
+        $.specctrPsDimension.createDimensionSpecsForItem();
+    },
+    
+    //Call create spacing spec method from specctrPsSpacing jsx file.
+    createSpacingSpecs : function() {
+        $.specctrPsSpacing.createSpacingSpecs();
+    },
+
+    //Call create coordinate spec method from specctrPsCoordinates jsx file.
+    createCoordinateSpecs : function() {
+        $.specctrPsCoordinates.createCoordinateSpecs();
+    },
+
+    //Call create property spec method from specctrPsProperties jsx file.
+    createPropertySpecs : function() {
+        $.specctrPsProperties.createPropertySpecsForItem();
+    },
+
+    //Call create exportCss method from specctrPsExportCss jsx file.
+    exportCss : function() {
+        $.specctrPsExportCss.exportCss();
+    }
+};
