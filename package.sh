a862382da8ed0ce6d0b994146e12543cbe3c8272
Dmitriy Fabrikant <mityaf@gmail.com>
2015-04-15 22:34:31 -0400

    package and sign script

---
+++ package.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+ROOT="/Users/${USER}/Dropbox"
+version="3.0.1.m"
+
+java  -jar "${ROOT}/signingtoolkit/ucf.jar" \
+      -package \
+      -storetype "PKCS12" \
+      -keystore "${ROOT}/certs/ComodoSpecctr.p12" \
+      -storepass "curses77" \
+      -tsa "https://timestamp.geotrust.com/tsa" \
+      "./Specctr-${version}.zxp" -C "./ExtensionContent" .
