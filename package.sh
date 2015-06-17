8a6b46180aea2e5edfefc52539303939aa37cbef
Dmitriy Fabrikant <mityaf@gmail.com>
2015-06-16 21:15:16 -0400

    update package command

--- package.sh
+++ package.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-ROOT="/Users/${USER}/Dropbox"
-version="${1}m"
+version="${1}.m"
 
-java  -jar "${ROOT}/signingtoolkit/ucf.jar" \
-      -package \
-      -storetype "PKCS12" \
-      -keystore "${ROOT}/certs/ComodoSpecctr.p12" \
-      -storepass "curses77" \
-      -tsa "https://timestamp.geotrust.com/tsa" \
-      "./Specctr-${version}.zxp" -C "./ExtensionContent" .
+~/Dropbox/certs/ZXPSignCmd_debug -sign ./ExtensionContent ./Specctr-${version}.zxp ~/Dropbox/certs/ComodoSpecctr.p12  curses77
