a1b08b99cd5c26513777a25c6ae0f0b433ca4de0
Dmitriy Fabrikant <mityaf@gmail.com>
2016-07-06 00:53:27 -0400

    Revert "Merge branch 'master' of https://github.com/Specctr/specctrPsHtml"
    This reverts commit 25ddf4a4b8126a7861d2fe69ce60c4d43310f05f, reversing
    changes made to 8f6f1cdd7a36f8377b06bcb16c4b61306b42c139.

--- package.sh
+++ package.sh
@@ -2,4 +2,3 @@
 version="${1}.m"
 
 ~/Dropbox/certs/ZXPSignCmd_debug -sign ./ExtensionContent ./Specctr-${version}.zxp ~/Dropbox/certs/ComodoSpecctr.p12  curses77
-cp -f ./Specctr-${version}.zxp ~/Dropbox/osxInstaller/SpecctrMaster/CommonFiles/SpecctrMaster.zip 
