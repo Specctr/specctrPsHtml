ecfc3d6f6cb2ad4cb27471035d230d27fc2829e1
Dmitriy Fabrikant <mityaf@gmail.com>
2016-09-24 11:42:30 -0400

    send host to server

--- ExtensionContent/controller/api/cloudUploadWS.js
+++ ExtensionContent/controller/api/cloudUploadWS.js
@@ -125,6 +125,7 @@ Specctr.cloudAPI = {
 			document_name: css.document_name,
 			css_items: cssJson.children,
 			project_name: projectName,
+            application: hostApplication
 		});
 
 		$.ajax({
@@ -182,7 +183,8 @@ Specctr.cloudAPI = {
 			machine_id: machine_id,
 			document_name: css.document_name,
 			css_items: cssJson.children,
-			project_id: response.project_id
+			project_id: response.project_id,
+            application: hostApplication
 		});
 
 		$.ajax({
